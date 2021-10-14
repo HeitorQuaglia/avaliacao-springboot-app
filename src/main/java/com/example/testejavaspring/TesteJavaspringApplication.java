8ed69c13b7108443ee6a71c9dedaf2f2120b1919
Heitor Quaglia <heitor@venturelabs.group>
2021-10-14 16:47:11 -0300

    first commit

---
+++ src/main/java/com/example/testejavaspring/TesteJavaspringApplication.java
@@ -0,0 +1,11 @@
+package com.example.testejavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteJavaspringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TesteJavaspringApplication.class, args);
+    }
+}
