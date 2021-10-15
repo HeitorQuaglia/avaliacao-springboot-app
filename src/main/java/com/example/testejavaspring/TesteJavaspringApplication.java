4e5cd5ae5e259d0cacd5d5ab2cbe5587d02e0979
Heitor Quaglia <heitor@venturelabs.gruop>
2021-10-14 22:45:19 -0300

    add properties and implements command line runner

--- src/main/java/com/example/testejavaspring/TesteJavaspringApplication.java
+++ src/main/java/com/example/testejavaspring/TesteJavaspringApplication.java
@@ -1,11 +1,17 @@
 package com.example.testejavaspring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TesteJavaspringApplication {
+public class TesteJavaspringApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(TesteJavaspringApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        //TODO...
     }
 }
